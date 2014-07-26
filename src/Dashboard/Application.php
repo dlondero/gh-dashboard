52bbe8c84c1f25016f7567e7793a00ff4557016e
Daniel Londero <daniel.londero@cargomedia.ch>
2014-07-26 11:42:47 +0200

    Add docblock

--- src/Dashboard/Application.php
+++ src/Dashboard/Application.php
@@ -1,5 +1,14 @@
 <?php
 
+/**
+ * This file is part of gh-dashboard.
+ *
+ * (c) Daniel Londero <daniel.londero@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Gh\Dashboard;
 
 use Symfony\Component\Console\Application as ConsoleApplication;
